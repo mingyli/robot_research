a1c6f69c269f804cc46a68d4065b235a53f417f1
roboboys <mingyli34@gmail.com>
2017-03-08 18:27:42 -0800

    subscribe listen transform mocap to baxter

--- tf_test.py
+++ tf_test.py
@@ -3,6 +3,7 @@ import rospy
 import baxter_interface
 from baxter_interface import Gripper
 import tf
+from sensor_msgs.msg import PointCloud
 
 
 rospy.init_node('Quickstart')
@@ -12,22 +13,27 @@ left = baxter_interface.Limb('left')
 right_gripper = Gripper('right')
 left_gripper = Gripper('left')
 
+def callback(data):
+	# print data
+	print listener.transformPointCloud('/left_lower_forearm', data)
+
 #start the listener
 listener = tf.TransformListener()
+rospy.Subscriber('mocap_point_cloud', PointCloud, callback)
+rospy.spin()
+# rospy.sleep(2) # waits for data to be published to topic. tf will grab last-published position
 
-
-rospy.sleep(2) # waits for data to be published to topic. tf will grab last-published position
-
-while True:
-	raw_input("Press Enter for transform coords")
-	while True:
-		try:
-			(trans,rot) = listener.lookupTransform('/right_gripper', '/base', rospy.Time(0))
-			# Look up the position of the right gripper w.r.t. base
-			# ((x, y, z), (x, y, z, w))
-			angles = right.joint_angles()
-			break
-		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-		    continue
-	print((trans, rot))
-	print angles
+# while True:
+# 	raw_input("Press Enter for transform coords")
+# 	while True:
+# 		try:
+# 			# (trans,rot) = listener.lookupTransform('/right_gripper', '/base', rospy.Time(0))
+# 			# Look up the position of the right gripper w.r.t. base
+# 			# ((x, y, z), (x, y, z, w))
+# 			# angles = right.joint_angles()
+			
+# 			break
+# 		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+# 		    continue
+# 	print((trans, rot))
+# 	print angles
